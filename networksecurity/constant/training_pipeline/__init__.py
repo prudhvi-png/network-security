6bf4891ef347e755771ba38fd3bf4031afc23298
prudhvi ganesh <prudhviredrouth143@gmail.com>
2025-07-21 08:24:07 +0530

    data validation

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,6 +16,8 @@ FILE_NAME:str = 'phishingData.csv'
 TRAIN_FILE_NAME:str = 'train.csv'
 TEST_FILE_NAME:str = 'test.csv'
 
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yml")
+
 
 '''
 Data Ingestion related constant start with DATA_INGESTION var Name
@@ -28,3 +30,12 @@ DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature store"
 DATA_INGESTION_INGESTED_DIR:str = "ingested"
 DATA_INGESTED_TRAIN_TEST_SPLIT_RATIO:float=0.2
+
+
+'''Data validation related constant start with DATA_VALIDATION VAR NAME'''
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR:str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yml"
